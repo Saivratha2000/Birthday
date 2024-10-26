8d591b7d6b445fad14796374b744d8ee6727d6de
Saivratha2000 <66941326+Saivratha2000@users.noreply.github.com>
2024-10-26 15:25:36 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById("playButton").addEventListener("click", () => {
+    alert("Play a personalized video message here!");
+    // If you have a video file, replace with code to play the video
+});
